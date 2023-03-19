040e5decbe42c260a38f0d4dd870fa0231cd9ad5
shimakaze <shimakaze@shimakaze.com>
2023-03-19 21:04:33 +0800

    在WebConfig添加处理中文乱码的方法

--- src/main/java/com/shimakaze/config/WebConfig.java
+++ src/main/java/com/shimakaze/config/WebConfig.java
@@ -1,8 +1,13 @@
 package com.shimakaze.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * @author: Sh1maka2E
@@ -17,5 +22,15 @@ public class WebConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/knowledge-graph/**").addResourceLocations("classpath:/knowledge-graph/");
+    }
+
+    /**
+     * 解决controller返回中文乱码
+     * @param converters
+     */
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        converters.add(converter);
     }
 }
