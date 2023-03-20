2161b10ca740306cd9185ec44beea0e21d296206
shimakaze <shimakaze@shimakaze.com>
2023-03-20 23:47:25 +0800

    用el-descriptions展示点击节点弹出属性功能，并用@JsonIgnore隐藏id

--- src/main/java/com/shimakaze/common/CommonNode.java
+++ src/main/java/com/shimakaze/common/CommonNode.java
@@ -1,6 +1,8 @@
 package com.shimakaze.common;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import org.neo4j.ogm.annotation.GraphId;
@@ -10,10 +12,11 @@ import org.neo4j.ogm.annotation.GraphId;
  * 公共节点类
  */
 @Data
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class CommonNode {
 
     @GraphId
+    @JsonIgnore //id不需要返回给前端
     protected Long id;
+    @JsonProperty(value = "名称") //返回给前端的属性名称
     protected String name;
 }
